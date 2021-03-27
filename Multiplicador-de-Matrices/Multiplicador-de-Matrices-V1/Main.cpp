5a4ad19106b977dda4f9fb64d6791efa4fa38740
Basilio Matijasevich, Agustín <81303198+Agustin-Basilio-Matijasevich@users.noreply.github.com>
2021-03-27 01:01:28 -0300

    Estuve trabajando en el Main

--- Multiplicador-de-Matrices/Multiplicador-de-Matrices-V1/Main.cpp
+++ Multiplicador-de-Matrices/Multiplicador-de-Matrices-V1/Main.cpp
@@ -18,6 +18,44 @@ using namespace std;
 
 int main()
 {
-	cout << "Hola" << endl;
+	//Variables de la funcion
+	int FMA, CMA, FMB, CMB;
+
+	//Banderas del Ciclo principal
+	bool Bandera = true;
+	int opcion = 1;
+
+	//Ciclo principal de funcionamiento
+	while (Bandera == true)
+	{
+		//Solicitud de datos
+		cout << "--Ingresemos la primera matriz--" << endl << endl;
+		cout << "Ingrese la cantidad de filas de la matriz A: ";
+		cin >> FMA;
+		cout << "Ingrese la cantidad de columnas de la matriz A: ";
+		cin >> CMA;
+
+		cout << "--A continuacion ingrese los valores de la matriz A en orden F1 y C1 luego F1 y C2 y asi sucesivamente--";
+
+		//Control del Ciclo principal
+		cout << "Ingrese el numero de la opcion que quiera:" << endl << "Opcion 1: Salir" << endl << "Opcion 2: Realizar otra multiplicacion" << endl << "Su opcion es: ";
+		cin >> opcion;
+
+		if (opcion == 1)
+		{
+			Bandera = false;
+		}
+		else if (opcion == 2)
+		{
+			Bandera = true;
+		}
+		else
+		{
+			cout << "Fatal Error in Main Cycle" << endl << endl;
+			system("Pause");
+			break;
+		}
+	}
+
 	return 0;
 }
