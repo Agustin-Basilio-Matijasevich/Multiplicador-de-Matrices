cbe6d0f3f17b4bda999a018b087593d8535d2788
Basilio Matijasevich, Agustín <81303198+Agustin-Basilio-Matijasevich@users.noreply.github.com>
2021-03-26 22:59:27 -0300

    Se creo la solucion y proyecto base
    Se creo la solucion y el proyecto de base para la multiplicadora de matrices

---
+++ Multiplicador-de-Matrices/Multiplicador-de-Matrices-V1/Main.cpp
@@ -0,0 +1,23 @@
+//Librerias
+#include "Librerias.hpp"
+
+//Espacios de memoria universales
+#include "Variables-Universales.hpp"
+#include "Constantes-Universales.hpp"
+
+//Funciones-Auxiliares
+#include "Funciones-Auxiliares.hpp"
+
+//Espacio numerico general
+using namespace std;
+
+//Funciones Auxiliares
+
+
+//Main
+
+int main()
+{
+	cout << "Hola" << endl;
+	return 0;
+}
